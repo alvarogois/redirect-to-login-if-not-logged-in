e6f35af1c9b21ac08b038e83a80a308f70e1ddbd
Daan Kortenbach <daan@kortenbach.org>
2012-10-12 11:30:06 +0200

    first commit

---
+++ dmk-redirect-if-not-logged-in.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Redirect if not logged in
+ * @version 0.1
+ */
+/*
+Plugin Name: Redirect if not logged in
+Plugin URI: 
+Description: Redirect to wp-login.php if user is not logged in.
+Author: Daan Kortenbach
+Version: 0.1
+Author URI: http://krtnb.ch/
+*/
+
+add_action( 'wp', 'dmk_not_loggedin_redirect' );
+
+/**
+ * Redirect to wp-login.php if user is not logged in.
+ *
+ * @author Daan Kortenbach
+ * 
+ * @global string $pagenow
+ * @return void redirect
+ */
+function dmk_not_loggedin_redirect() {
+
+	global $pagenow;
+
+	if(!is_user_logged_in() && $pagenow != 'wp-login.php')
+		wp_redirect( home_url() . '/wp-login.php', 302 );
+
+}
