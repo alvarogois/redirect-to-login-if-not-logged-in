8cb6a14d629943447ea86d40915e6e01eb865c48
Daan Kortenbach <daan@kortenbach.org>
2013-05-29 11:26:58 +0200

    Bump to 1.0
    Signed-off-by: Daan Kortenbach <daan@kortenbach.org>

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
-Redirect if not logged in
-==============
+# Redirect to login if not logged in #
+**Contributors:** daankortenbach
+**Donate link:** https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BKUCJXJJ8XJVY
+**Tags:** redirect, login
+**Requires at least:** 3.0.1
+**Tested up to:** 3.6
+**Stable tag:** 1.0
+**License:** GPLv2 or later
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html
 
-Redirect to wp-login.php if user is not logged in
---------------
+Redirects site visitors to the login page if the visitor is not logged in.
 
-Simple WordPress plugin to redirect users to wp-login.php if they are not logged in. Usefull if a site is in development or for test servers... No more peeking :)
+## Description ##
+
+Redirects site visitors to the login page if the visitor is not logged in. This plugin is used fairly often for sites in development or sites only available for registered users.
+
+## Installation ##
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+## Changelog ##
+
+### 1.0 ###
+* Initial version.
--- dmk-redirect-if-not-logged-in.php
+++ dmk-redirect-if-not-logged-in.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * @package Redirect if not logged in
- * @version 0.1
+ * @package Redirect to login if not logged in
+ * @version 1.0
  */
 /*
-Plugin Name: Redirect if not logged in
-Plugin URI: 
-Description: Redirect to wp-login.php if user is not logged in.
+Plugin Name: Redirect to login if not logged in
+Plugin URI: http://daan.kortenba.ch/redirect-to-wp-login-php-if-user-is-not-logged-in/
+Description: Redirects site visitors to the login page if the visitor is not logged in. This plugin is used fairly often for sites in development or sites only available for registered users.
 Author: Daan Kortenbach
-Version: 0.1
-Author URI: http://krtnb.ch/
+Version: 1.0
+Author URI: http://daan.kortenba.ch/
 */
 
-add_action( 'wp', 'dmk_not_loggedin_redirect' );
+add_action( 'wp', 'dk_redirect_to_login_if_not_logged_in', 1 );
 
 /**
  * Redirect to wp-login.php if user is not logged in.
  *
  * @author Daan Kortenbach
- * 
+ *
  * @global string $pagenow
- * @return void redirect
+ * @return void redirect to login.php
  */
-function dmk_not_loggedin_redirect() {
+function dk_redirect_to_login_if_not_logged_in() {
 
 	global $pagenow;
 
-	if(!is_user_logged_in() && $pagenow != 'wp-login.php')
-		wp_redirect( home_url() . '/wp-login.php', 302 );
-
+	// If the visitor is not logged in, redirect to login url
+	if ( ! is_user_logged_in() && $pagenow != 'wp-login.php' )
+		wp_redirect( wp_login_url() );
 }
